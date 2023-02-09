7605b148f914b887dead3f6286d5f701e16f6ce6
Yatharth arora <yathartharora1999@gmail.com>
2023-02-09 14:58:26 -0800

    Create index.js

---
+++ src/menu-items/index.js
@@ -0,0 +1,11 @@
+import pages from './pages';
+import utilities from './utilities';
+import other from './other';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+    items: [pages, utilities, other]
+};
+
+export default menuItems;
