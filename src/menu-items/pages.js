6a120073c831bf864eb4195a2d1c2089c85066aa
Yatharth arora <yathartharora1999@gmail.com>
2023-02-09 14:58:46 -0800

    Menu-items

---
+++ src/menu-items/pages.js
@@ -0,0 +1,39 @@
+// assets
+import { IconKey } from '@tabler/icons';
+
+// constant
+const icons = {
+    IconKey
+};
+
+// ==============================|| EXTRA PAGES MENU ITEMS ||============================== //
+
+const pages = {
+    type: 'group',
+    children: [
+        {
+            id: 'authentication',
+            title: 'Manage',
+            type: 'collapse',
+
+            children: [
+                {
+                    id: 'login3',
+                    title: 'Login',
+                    type: 'item',
+                    url: '/pages/login/login3',
+                    target: true
+                },
+                {
+                    id: 'register3',
+                    title: 'Register',
+                    type: 'item',
+                    url: '/pages/register/register3',
+                    target: true
+                }
+            ]
+        }
+    ]
+};
+
+export default pages;
